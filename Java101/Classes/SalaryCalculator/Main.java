480931bb3466336bd0f74bce07f7b7f2e713073e
AllDev0 <helloworlddeveloper0@gmail.com>
2024-01-08 23:24:51 +0300

    SalaryCalculator added

---
+++ Java101/Classes/SalaryCalculator/Main.java
@@ -0,0 +1,8 @@
+package Java101.Classes.SalaryCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Enes", 35000, 45, 2000);
+        employee.toString();
+    }
+}
